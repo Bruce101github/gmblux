9ea49187aabc202bf61855f54356f1aeba04de84
Bruce101github <brucethiombiano@gmail.com>
2025-10-17 16:58:52 +0000

    feat(ui): make navbar dynamic, add property pages, and implement share + carousel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import PropertyLising from "./pages/PropertyListing";
+import Property from "./pages/Property";
 import Navbar, { MobileNavbar } from "./components/Navbar";
 import Footer, { MobileFooter } from "./components/Footer";
 import { useMediaQuery } from "react-responsive";
+import ScrollToTop from "@/components/ScrollToTop";
 
 function App() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
   return (
     <main className="w-[100vw] bg-[#121420] h-auto overflow-x-hidden">
       {isMobile ? <MobileNavbar /> : <Navbar />}
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/listings" element={<PropertyLising />} />
+        <Route path="/listing/:id" element={<Property />} />
       </Routes>
       {isMobile ? <MobileFooter /> : <Footer />}
     </main>
