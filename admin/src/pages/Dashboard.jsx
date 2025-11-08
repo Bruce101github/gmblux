f616b6f8f3d236ca721669cd74a643085ba55c28
Bruce101github <brucethiombiano@gmail.com>
2025-11-08 04:32:23 +0000

    feat(pages): enhance Dashboard and AddProperties pages with responsive layout and image removal functionality

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Table } from "@/components/ui/table";
 import { DataTable } from "@/components/ui/DataTable";
-import { ChartBarMultiple } from "@/components/ui/ChartBarMultiple";
+import { ChartAreaInteractive } from "@/components/ui/ChartAreaInteractive";
 import { columns } from "@/pages/bookings/columns";
 import Calendar14 from "../components/calendar-14.jsx";
 import { sampleBookings } from "@/pages/bookings/sampleData"; // your booking data
@@ -12,6 +12,8 @@ import {
   CircleFadingPlus,
   BadgeCent,
   UsersRound,
+  TrendingUp,
+  TrendingDown,
 } from "lucide-react";
 
 import {
@@ -23,12 +25,20 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Calendar31 from "@/components/calendar-31.jsx";
 
 export default function Dashboard() {
   const [bookings, setBookings] = useState([]);
+  const [properties, setProperties] = useState([]);
+  const [tableInfo, setTableInfo] = useState([]);
+  const [newRequest, setNewRequest] = useState("");
+  const [totalRequest, setTotalRequest] = useState("");
+  const [upcomingTour, setUpcomingTour] = useState("");
+  const [canceled, setCanceled] = useState("");
+
   useEffect(() => {
     async function fetchBookings() {
-      const { data, error } = await supabase.from("properties").select("*");
+      const { data, error } = await supabase.from("booking").select("*");
 
       if (error) {
         console.error("Error fetching bookings:", error);
@@ -37,66 +47,136 @@ export default function Dashboard() {
         console.log(data);
       }
     }
+
+    async function fetchProperties() {
+      const { data: properties, error } = await supabase
+        .from("properties")
+        .select("*");
+
+      if (error) {
+        console.error("Error fetching properties:", error);
+      } else {
+        setProperties(properties);
+        console.log(properties);
+      }
+    }
     fetchBookings();
+    fetchProperties();
   }, []);
+
+  useEffect(() => {
+    const merged = [];
+
+    bookings.forEach((booking) => {
+      const match = properties.find(
+        (property) => booking.property_id === property.id,
+      );
+      if (match) {
+        merged.push({ ...booking, ...match });
+      }
+    });
+
+    setTableInfo(merged);
+  }, [bookings]);
+  const matches = bookings.filter((booking) => booking.status === "pending");
+  const tours = bookings.filter(
+    (booking) =>
+      booking.request_type === "Tour" && booking.status === "scheduled",
+  );
+  const canceledBookings = bookings.filter(
+    (booking) => booking.status === "canceled",
+  );
+
+  useEffect(() => {
+    setNewRequest(matches.length);
+    setTotalRequest(bookings.length);
+    setUpcomingTour(tours.length);
+    setCanceled(canceledBookings.length);
+  }, [bookings]);
+
   return (
-    <Card className="w-full min-h-[200vh] bg-white p-8 bg-fixed">
-      <h1 className="text-xl font-bold">Dashboard</h1>
-      <div className="grid grid-cols-4 gap-x-5">
-        <Card className="p-0 gap-1 bg-[#F4F4F8]">
+    <>
+      <h1 className="text-lg font-medium lg:text-xl mx-[5%]  lg:font-bold mt-[-48px] lg:mt-4 mb-2 text-white">
+        Dashboard
+      </h1>
+      <div className="grid lg:grid-cols-4 lg:gap-4 gap-2 lg:gap-4 grid-cols-2 lg:mr-10 px-[5%] lg:px-0 justify-start items-start">
+        <Card className="p-0 gap-1 bg-[#121420] border-white/10">
           <Card className="h-auto gap-1 p-4">
-            <div className="h-[32px] w-[32px] bg-yellow-400 rounded-sm p-[4px]">
-              <CircleFadingPlus color="white" size={24} />
+            <div className="h-[32px] w-[32px] bg-yellow-300/30 rounded-sm p-[4px]">
+              <CircleFadingPlus color="yellow" size={24} />
             </div>
-            <h2 className="text-gray-500 font-bold text-lg">
-              Upcoming Bookings
-            </h2>
-            <p className="text-2xl font-md">{bookings.length}</p>
+            <h2 className="text-white/60 font-bold text-md">New Request</h2>
+            <p className="text-2xl font-bold text-white">{newRequest}</p>
           </Card>
-          <div className="h-[40px]"></div>
+          <div className="h-[30px] flex px-2 lg:px-5 gap-2 items-center">
+            <div className="flex gap-1 bg-[#93FF96]/10 px-1.5 rounded-3xl">
+              <TrendingUp color="#93FF96" size={18} />{" "}
+              <p className="text-[#93FF96]  text-xs lg:text-sm">17%</p>
+            </div>
+            <p className="text-white/60 text-xs lg:text-sm">vrs last month</p>
+          </div>
         </Card>
-        <Card className="p-0 gap-1  bg-[#F4F4F8]">
+        <Card className="p-0 gap-1 bg-[#121420] border-white/10">
           <Card className="h-auto gap-1 p-4">
-            <div className="h-[32px] w-[32px] bg-yellow-400 rounded-sm p-[4px]">
-              <CalendarPlus color="white" size={24} />
+            <div className="h-[32px] w-[32px] bg-yellow-300/30 rounded-sm p-[4px]">
+              <CalendarPlus color="yellow" size={24} />
             </div>
-            <h2 className="text-gray-500 font-bold text-sm">Total Bookings</h2>
-            <p className="text-2xl font-md">{bookings.length}</p>
+            <h2 className="text-white/60 font-bold text-md">Total Request</h2>
+            <p className="text-2xl font-bold text-white">{totalRequest}</p>
           </Card>
-          <div className="h-[40px]"></div>
+          <div className="h-[30px] flex   px-2 lg:px-5 gap-2 items-center">
+            <div className="flex gap-1 bg-[#93FF96]/10 px-1.5 rounded-3xl">
+              <TrendingUp color="#93FF96" size={18} />{" "}
+              <p className="text-[#93FF96] text-xs lg:text-sm">17%</p>
+            </div>
+            <p className="text-white/60 text-xs lg:text-sm">vrs last month</p>
+          </div>
         </Card>
-        <Card className="p-0 gap-1  bg-[#F4F4F8]">
+        <Card className="p-0 gap-1 bg-[#121420] border-white/10">
           <Card className="h-auto gap-1 p-4">
-            <div className="h-[32px] w-[32px] bg-yellow-400 rounded-sm p-[4px]">
-              <BadgeCent color="white" size={24} />
+            <div className="h-[32px] w-[32px] bg-yellow-300/30 rounded-sm p-[4px]">
+              <BadgeCent color="yellow" size={24} />
             </div>
-            <h2 className="text-gray-500 font-bold text-sm">Total Revenue</h2>
-            <p className="text-2xl font-md">GH₵120,000</p>
+            <h2 className="text-white/60 font-bold text-md">Upcoming Tour</h2>
+            <p className="text-2xl font-bold text-white">{upcomingTour}</p>
           </Card>
-          <div className="h-[40px]"></div>
+          <div className="h-[30px] flex  px-2 lg:px-5 gap-2 items-center">
+            <div className="flex gap-1 bg-red-400/10 px-1.5 rounded-3xl">
+              <TrendingDown color="red" size={18} />{" "}
+              <p className="text-red-500 text-xs lg:text-sm">6%</p>
+            </div>
+            <p className="text-white/60 text-xs lg:text-sm">vrs last month</p>
+          </div>
         </Card>
-        <Card className="p-0 gap-1  bg-[#F4F4F8]">
+        <Card className="p-0 gap-1 bg-[#121420] border-white/10">
           <Card className="h-auto gap-1 p-4">
-            <div className="h-[32px] w-[32px] bg-yellow-400 rounded-sm p-[4px]">
-              <UsersRound color="white" size={24} />
+            <div className="h-[32px] w-[32px] bg-yellow-300/30 rounded-sm p-[4px]">
+              <UsersRound color="yellow" size={24} />
             </div>
-            <h2 className="text-gray-500 font-bold text-sm">Total Customers</h2>
-            <p className="text-2xl font-md">{bookings.length}</p>
+            <h2 className="text-white/60 font-bold text-md">Canceled</h2>
+            <p className="text-2xl font-bold text-white">{canceled}</p>
           </Card>
-          <div className="h-[40px]"></div>
+          <div className="h-[30px] flex  px-2 lg:px-5 gap-2 items-center">
+            <div className="flex gap-1 bg-[#93FF96]/10 px-1.5 rounded-3xl">
+              <TrendingUp color="#93FF96" size={18} />{" "}
+              <p className="text-[#93FF96] text-xs lg:text-sm">17%</p>
+            </div>
+            <p className="text-white/60  text-xs lg:text-sm">vrs last year</p>
+          </div>
         </Card>
       </div>
-      <Card className="px-4 py-4">
-        <div className="flex gap-x-4 grid-cols-3">
-          <Calendar14 />
+      <div className="w-full py-4 lg:grid flex flex-col-reverse lg:grid-cols-[70%_34%] gap-2 lg:gap-x-4 px-[5%] lg:mr-[5%] lg:px-[0]">
+        <div className="">
+          <div className="w-full mb-4">
+            <ChartAreaInteractive />
+          </div>
+          <Card className="hidden lg:block p-4">
+            <p className="text-lg font-bold text-white">Booking</p>
+            <DataTable columns={columns} data={tableInfo} />
+          </Card>
         </div>
-        <span>
-          {" "}
-          <p className="text-lg font-bold">Booking</p>
-        </span>
-
-        <DataTable columns={columns} data={bookings} />
-      </Card>
-    </Card>
+        <Calendar31 className="" />
+      </div>
+    </>
   );
 }
