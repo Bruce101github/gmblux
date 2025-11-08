a6d9880e681d170166e236766ef09181681dfb87
Bruce101github <brucethiombiano@gmail.com>
2025-11-08 04:22:12 +0000

    feat(ui): enhance AddProperties page with responsiveness, image preview, and delete functionality

--- admin/src/layouts/DashboardLayouts.jsx
+++ admin/src/layouts/DashboardLayouts.jsx
@@ -13,6 +13,7 @@ import {
   BadgeQuestionMark,
   PanelLeftClose,
   CircleFadingPlus,
+  Menu,
 } from "lucide-react";
 import { supabase } from "../lib/supabaseClient";
 import { useNavigate } from "react-router-dom";
@@ -25,12 +26,12 @@ import {
   SidebarContent,
   SidebarGroup,
   SidebarGroupContent,
-  SidebarGroupLabel,
   SidebarMenu,
   SidebarMenuItem,
   SidebarMenuButton,
   SidebarFooter,
   SidebarInput,
+  SidebarTrigger,
 } from "@/components/ui/sidebar";
 
 import { NavLink } from "react-router-dom";
@@ -117,7 +118,7 @@ export default function DashboardLayout({ children }) {
     <SidebarProvider>
       <div className="flex h-screen w-screen">
         {/* Sidebar */}
-        <Sidebar>
+        <Sidebar collapsible="icon">
           <SidebarHeader>
             <div className="h-[45px] w-auto mt-[2.5vh] flex justify-between items-center">
               <div className="flex gap-x-3 items-center">
@@ -128,19 +129,22 @@ export default function DashboardLayout({ children }) {
                     className="w-full h-full objet-over rounded-sm"
                   />
                 </div>
-                <h2 className="text-xl font-bold">GMB LUX</h2>
+                <h2 className="text-xl font-bold text-white">GMB LUX</h2>
               </div>
-              <div>
-                <PanelLeftClose size={18} />
-              </div>
+              <SidebarTrigger className="!text-white/80">
+                <SidebarMenuButton>
+                  <PanelLeftClose color="white" size={18} />
+                </SidebarMenuButton>
+              </SidebarTrigger>
             </div>
           </SidebarHeader>
           <SidebarContent>
             <SidebarGroup>
-              <div className="relative my-3">
+              <div className="relative my-3 ">
                 <Search
                   className="absolute top-5 right-2 transform -translate-y-2/3"
                   size={18}
+                  color="white"
                 />
                 <SidebarInput className="pr-8" />
               </div>
@@ -184,7 +188,7 @@ export default function DashboardLayout({ children }) {
                 </SidebarMenuItem>
               </SidebarMenu>
             </SidebarGroupContent>
-            <div className="h-[1px] w-full bg-gray-300"></div>
+            <div className="h-[1px] w-full bg-white/10"></div>
             <div className="flex gap-2 py-2">
               <div className="h-[35px] w-[35px]">
                 <img
@@ -193,17 +197,20 @@ export default function DashboardLayout({ children }) {
                 />
               </div>
               <div>
-                <h3 className="text-sm font-bold">
+                <h3 className="text-sm font-bold text-white">
                   {profile?.first_name} {profile?.last_name}
                 </h3>
-                <h3 className="text-xs">{user.email}</h3>
+                <h3 className="text-xs text-white">{user.email}</h3>
               </div>
             </div>
           </SidebarFooter>
         </Sidebar>
         {/* Main content */}
-        <main className="w-full min-h-[150vh] p-6 bg-[#F4F4F8]">
-          {" "}
+        <main className="w-full min-h-[150vh] lg:p-6 bg-[#121420]">
+          <div className="lg:hidden flex justify-between px-[5%]">
+            <div></div>
+            <SidebarTrigger mobile={true} className="!text-white/80 my-5" />
+          </div>{" "}
           <Outlet />
         </main>
       </div>
--- admin/src/pages/AddProperties.jsx
+++ admin/src/pages/AddProperties.jsx
@@ -1,10 +1,12 @@
 import { Card } from "@/components/ui/card";
-import DropArea from "@/components/DropArea";
+import DropArea, { DropButton } from "@/components/DropArea";
 import { useState, useEffect, useRef } from "react";
 import { supabase } from "@/lib/supabaseClient";
+import { X } from "lucide-react";
 
 function AddProperties() {
   const [files, setFiles] = useState([]);
+  const [focus, setFocus] = useState(0);
   const [title, setTitle] = useState("");
 
   const [formData, setFormData] = useState({
@@ -70,12 +72,46 @@ function AddProperties() {
     }
   }
 
+  const deleteImg = (index) => {
+    setFiles((prev) => {
+      const newFiles = [...prev];
+
+      setFocus((prevFocus) => {
+        // If no files left, clear focus
+        if (newFiles.length === 0) return null;
+        // If prevFocus is beyond last index, move focus back
+        if (prevFocus >= newFiles.length) return newFiles.length - 1;
+        // Otherwise keep same focus
+        return prevFocus;
+      });
+
+      newFiles.splice(index, 1);
+      return newFiles;
+    });
+  };
+  const handlePreview = (index) => {
+    const deleteButton = document.getElementById(`${index}`);
+    setFocus(index);
+    if (window.innerWidth <= 640) {
+      files.map((file, i) => {
+        const fileDel = document.getElementById(`${i}`);
+        fileDel.classList.add("hidden");
+      });
+      deleteButton.classList.toggle("hidden");
+      setTimeout(() => {
+        deleteButton.classList.add("hidden");
+      }, 1500);
+    }
+  };
+
   return (
-    <Card className="w-full min-h-[100vh] bg-white p-8 ">
-      <div className="flex justify-between">
+    <>
+      <div className="flex justify-between px-[5%] md:px-0 lg:px-0 mt-[-48px] lg:mt-4">
         {" "}
-        <h2>Add New Property</h2>
-        <div className="flex gap-6">
+        <h2 className="text-white text-lg font-medium lg:text-xl lg:font-bold">
+          Add New Property
+        </h2>
+        <div className="hidden lg:flex lg:gap-6 gap-2">
           <button className="border border-gray-400 px-4 py-2 rounded-sm text-gray-500">
             Discard
           </button>
@@ -88,61 +124,87 @@ function AddProperties() {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-2 gap-x-4">
-        <Card className="p-4">
-          <p>Add images</p>
-          <DropArea setFiles={setFiles} />
-          <div className="grid grid-cols-3 gap-4">
-            {files.map((file) => (
-              <Card className="max-h-[200px] p-0">
+      <div className="grid lg:grid-cols-2 lg:gap-5 px-[5%]  lg:px-0 lg:py-0">
+        <Card className="p-4 bg-white/10 my-5">
+          {files.length > 0 ? (
+            <div className="w-full max-h-[40vh] lg:min-h-[400px] rounded-lg">
+              <img
+                src={files[focus].preview}
+                className="w-full h-full rounded-lg object-cover"
+              />
+            </div>
+          ) : (
+            <DropArea setFiles={setFiles} />
+          )}
+          <div className="grid grid-cols-3 gap-2 lg:gap-4">
+            {files.map((file, index) => (
+              <Card className="max-h-[100px]  lg:max-h-[200px] p-0 relative group">
+                <button
+                  id={index}
+                  className="hidden bg-red-400 p-1 rounded-full group-hover:block absolute right-0 mt-[-10px] mr-[-10px]"
+                  onClick={() => {
+                    deleteImg(index);
+                  }}
+                >
+                  <X color="white" size={24} />
+                </button>
                 <img
                   src={file.preview}
                   className="h-full object-cover rounded-lg"
+                  onClick={() => {
+                    handlePreview(index);
+                  }}
                 />
               </Card>
             ))}
+            {files.length > 0 ? <DropButton setFiles={setFiles} /> : null}
           </div>
         </Card>
-        <Card className="p-8">
+        <Card className="px-4 lg:p-8 bg-white/10 mb-5 lg:mt-5">
           <form
             id="propertyForm"
-            className="flex flex-col gap-y-5"
+            className="flex flex-col gap-y-5 bg-none"
             onSubmit={(e) => {
               e.preventDefault();
             }}
           >
-            <p>Property Details</p>
             <div className="flex flex-col">
-              <label htmlFor="title" className="text-gray-600">
+              <label htmlFor="title" className="text-white/60 mb-1 text-sm">
                 Property Name
               </label>
               <input
                 id="title"
                 name="title"
-                className="border-gray-300 border-1 rounded-sm p-2"
+                className="border-white/10 border-1 rounded-sm p-2 text-white"
                 value={formData.title}
                 onChange={handleState}
               />
             </div>
             <div className="flex flex-col">
-              <label htmlFor="description" className="text-gray-600">
+              <label
+                htmlFor="description"
+                className="text-white/60 mb-1 text-sm"
+              >
                 Description
               </label>
               <input
                 name="description"
-                className="border-gray-300 border-1 rounded-sm p-2"
+                className="border-white/10 border-1 rounded-sm p-2 text-white"
                 value={formData.description}
                 onChange={handleState}
               />
             </div>
-            <div className="flex grid grid-cols-2 gap-x-4">
+            <div className="flex grid grid-cols-2 lg:gap-x-4 gap-x-2">
               <div className="flex flex-col">
-                <label htmlFor="currency" className="text-gray-600">
+                <label
+                  htmlFor="currency"
+                  className="text-white/60 mb-1 text-sm"
+                >
                   Currency
                 </label>
                 <select
                   name="currency"
-                  className="w-full border-gray-300 border p-4.5 rounded-sm"
+                  className="w-full border-white/10 border p-4.5 rounded-sm text-white"
                   value={formData.currency}
                   onChange={handleState}
                 >
@@ -152,11 +214,11 @@ function AddProperties() {
               </div>
               <div className="flex flex-col">
                 {" "}
-                <label htmlFor="price" className="text-gray-600">
+                <label htmlFor="price" className="text-white/60 mb-1 text-sm">
                   Price
                 </label>
                 <input
-                  className="p-4 w-full border border-gray-300 rounded-sm"
+                  className="p-4 w-full border border-white/10 rounded-sm text-white"
                   type="number"
                   min="1"
                   name="price"
@@ -166,24 +228,27 @@ function AddProperties() {
               </div>
             </div>
             <div className="flex flex-col">
-              <label htmlFor="location" className="text-gray-600">
+              <label htmlFor="location" className="text-white/60 mb-1 text-sm">
                 Location
               </label>
               <input
                 name="location"
-                className="p-2 border-gray-200 border-1 rounded-sm"
+                className="p-2 border-white/10 border-1 rounded-sm text-white"
                 value={formData.location}
                 onChange={handleState}
               />
             </div>
-            <div className="flex grid grid-cols-2 gap-x-4">
+            <div className="flex grid grid-cols-2 lg:gap-x-4 gap-x-2">
               <div className="flex flex-col">
-                <label htmlFor="propertyType" className="text-gray-600">
+                <label
+                  htmlFor="propertyType"
+                  className="text-white/60 mb-1 text-sm"
+                >
                   Property Type
                 </label>
                 <select
                   name="property_type"
-                  className="w-full border-gray-300 border p-4 rounded-sm"
+                  className="w-full border-white/10 border p-4 rounded-sm text-white"
                   value={formData.property_type}
                   onChange={handleState}
                 >
@@ -203,12 +268,15 @@ function AddProperties() {
               </div>
               <div className="flex flex-col">
                 {" "}
-                <label htmlFor="listingType" className="text-gray-600">
+                <label
+                  htmlFor="listingType"
+                  className="text-white/60 mb-1 text-sm"
+                >
                   Listing Type
                 </label>
                 <select
                   name="listing_type"
-                  className="w-full border-gray-300 border p-4 rounded-sm"
+                  className="w-full border-white/10 border p-4 rounded-sm text-white"
                   value={formData.listing_type}
                   onChange={handleState}
                 >
@@ -217,14 +285,17 @@ function AddProperties() {
                 </select>
               </div>
             </div>
-            <div className="flex grid grid-cols-2 gap-x-4">
+            <div className="flex grid grid-cols-2 lg:gap-x-4 gap-x-2">
               <div className="flex flex-col">
-                <label htmlFor="bedrooms" className="text-gray-600">
+                <label
+                  htmlFor="bedrooms"
+                  className="text-white/60 mb-1 text-sm"
+                >
                   Bedrooms
                 </label>
                 <input
                   name="bedrooms"
-                  className="p-4 w-full border border-gray-300 rounded-sm"
+                  className="p-4 w-full border border-white/10 rounded-sm text-white"
                   type="number"
                   min="1"
                   max="20"
@@ -234,12 +305,15 @@ function AddProperties() {
               </div>
               <div className="flex flex-col">
                 {" "}
-                <label htmlFor="bathrooms" className="text-gray-600">
+                <label
+                  htmlFor="bathrooms"
+                  className="text-white/60 mb-1 text-sm"
+                >
                   Bathrooms
                 </label>
                 <input
                   name="bathrooms"
-                  className="p-4 w-full border border-gray-300 rounded-sm"
+                  className="p-4 w-full border border-white/10 rounded-sm text-white"
                   type="number"
                   min="1"
                   max="20"
@@ -250,8 +324,20 @@ function AddProperties() {
             </div>
           </form>
         </Card>
+        <div className="lg:hidden md:hidden flex gap-2 grid grid-cols-2 w-full mb-10">
+          <button className="border border-gray-400 px-4 py-2 rounded-sm text-gray-500">
+            Discard
+          </button>
+          <button
+            className="bg-yellow-400 px-4 py-2 rounded-sm text-white font-bold"
+            form="propertyForm"
+            onClick={insertProperty}
+          >
+            Publish
+          </button>
+        </div>
       </div>
-    </Card>
+    </>
   );
 }
 
